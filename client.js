6d3df2252cbdf6c0f0faf84a8b3f85d166ac33da
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-06 16:19:49 -0500

    submit btn functionality

--- client.js
+++ client.js
@@ -1,1 +1,13 @@
-console.log('hey');
+$(document).ready(onReady);
+
+function onReady() {
+$('#submitBtn').on('click', buttonClicked);
+
+}
+
+var total = 0;
+function buttonClicked() {
+  console.log('in buttonClicked');
+  //$('.displayInfo').append(.inputF)
+  console.log($('.inputF').val());
+}
