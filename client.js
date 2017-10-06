820a0a1bdee736205bfd716ef8869ca6e09a1ba2
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-06 18:39:58 -0500

    click to add table row

--- client.js
+++ client.js
@@ -9,9 +9,10 @@ var total = 0;
 function submitClicked() {
   console.log('in buttonClicked');
   $('.displayInfo').append("<p>" + $('#fName').val() + "</p>");
-  $('.displayInfo').append("<p>" + $('#lName').val() + "</p>");
 
-  $('table').append('<tr><td>' + $('#fName').val() + '</td></tr>');
 
-  console.log($('.inputF').val());
+  $('table').append('<tr><td>' + $('#fName').val() + '</td><td>' +
+  $('#lName').val() + '</td><td>'  + $('#iDNum').val() + '</td><td>' +
+  $('#jobTitle').val() + '</td><td>' + $('#salary').val() + '</td></tr>');
+
 }
