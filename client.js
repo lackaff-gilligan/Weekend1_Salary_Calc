5e90f8d2717e51e7e0ac068ebae06b61a32f41c2
Kevin Lackaff-Gilligan <kevin.lackaff.gilligan@gmail.com>
2017-10-06 19:12:36 -0500

    progress on monthAvg

--- client.js
+++ client.js
@@ -6,13 +6,26 @@ $('#submitBtn').on('click', submitClicked);
 }
 
 var total = 0;
+
 function submitClicked() {
   console.log('in buttonClicked');
   $('.displayInfo').append("<p>" + $('#fName').val() + "</p>");
+addTableRow();
+calculateTotal();
+resetFields();
+}
 
-
+function addTableRow() {
   $('table').append('<tr><td>' + $('#fName').val() + '</td><td>' +
   $('#lName').val() + '</td><td>'  + $('#iDNum').val() + '</td><td>' +
   $('#jobTitle').val() + '</td><td>' + $('#salary').val() + '</td></tr>');
+}
 
+function calculateTotal() {
+  total += parseInt($('#salary').val());
+  $('#monthAvg').text(total);
+}
+
+function resetFields() {
+  $('.inputs').val('');
 }
